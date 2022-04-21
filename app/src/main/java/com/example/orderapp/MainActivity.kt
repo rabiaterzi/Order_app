b4b5166aea96818f4c818f73afcc151b0eaaec9d
Rabia Terzi <terzirabia@hotmail.com>
2022-04-21 19:32:59 +0300

    Second Page

--- app/src/main/java/com/example/orderapp/MainActivity.kt
+++ app/src/main/java/com/example/orderapp/MainActivity.kt
@@ -2,10 +2,60 @@ package com.example.orderapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.content.Intent
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.EditText
+import android.widget.Toast
+import java.util.regex.Pattern
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val buttonClick = findViewById<Button>(R.id.button)
+        val checkbox = findViewById<CheckBox>(R.id.checkBox)
+        val email = findViewById<EditText>(R.id.editTextEmail)
+        val sifre = findViewById<EditText>(R.id.editTextSifre)
+        val ad = findViewById<EditText>(R.id.editTextAd)
+        val soyad = findViewById<EditText>(R.id.editTextSoyad)
+
+        fun isEmailValid(email: String): Boolean = Pattern.compile(
+                "[a-zA-Z0-9\\+\\.\\_\\%\\-\\+]{1,256}" +
+                        "\\@yalova.edu.tr"
+            ).matcher(email).matches()
+
+        buttonClick.setOnClickListener {
+            if(!email.text.isNullOrBlank()){
+                if(isEmailValid(email.text.toString())){
+                    if(!sifre.text.isNullOrBlank()){
+                        if(sifre.text.length >= 8){
+                            if(checkbox.isChecked){
+                                val ad_p = ad.text.toString()
+                                val soyad_p = soyad.text.toString()
+                                val email_p = email.text.toString()
+                                val intent = Intent(this, MenuActivity::class.java).apply {
+                                    putExtra("Ad", ad_p)
+                                    putExtra("Soyad", soyad_p)
+                                    putExtra("Email", email_p)
+                                }
+                                startActivity(intent)
+                            }else{
+                                Toast.makeText(baseContext, "KVKK metnini onaylamalısınız!", Toast.LENGTH_SHORT).show()
+                            }
+                        }else{
+                            Toast.makeText(baseContext, "Şifre en az 8 karakter olmalı!", Toast.LENGTH_SHORT).show()
+                        }
+                    }else{
+                        Toast.makeText(baseContext, "Şifre boş olamaz!", Toast.LENGTH_SHORT).show()
+                    }
+                }else{
+                    Toast.makeText(baseContext, "Email doğru formatta olmalı!", Toast.LENGTH_SHORT).show()
+                }
+            }else{
+                Toast.makeText(baseContext, "Email boş olamaz!", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
--- app/src/main/java/com/example/orderapp/MenuActivity.kt
+++ app/src/main/java/com/example/orderapp/MenuActivity.kt
@@ -1,11 +1,79 @@
 package com.example.orderapp
 
+import com.bumptech.glide.Glide
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.*
+import androidx.core.view.isVisible
+
+
 
 class MenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
+
+        val radioButton1 = findViewById<RadioButton>(R.id.radioButton1)
+        val radioButton2 = findViewById<RadioButton>(R.id.radioButton2)
+        val radioButton3 = findViewById<RadioButton>(R.id.radioButton3)
+        val radioButton4 = findViewById<RadioButton>(R.id.radioButton4)
+        val radioButton5 = findViewById<RadioButton>(R.id.radioButton5)
+        val radioButton6 = findViewById<RadioButton>(R.id.radioButton6)
+
+        val hesapla = findViewById<Button>(R.id.hesapla)
+        val ucret = findViewById<TextView>(R.id.ucret)
+        val ad_soyad = findViewById<TextView>(R.id.textViewPersonName)
+        val email = findViewById<TextView>(R.id.textViewPersonEmail)
+        val imageView = findViewById<ImageView>(R.id.imageView)
+
+        var toplam_ucret = 0
+        var isChecked = true
+
+        ucret.isVisible = false
+
+        ad_soyad.text = "Hoşgeldin "+ intent.getStringExtra("Ad") + " " + intent.getStringExtra("Soyad")
+        email.text = intent.getStringExtra("Email")
+
+        val icon = "https://cdn-icons-png.flaticon.com/512/747/747376.png"
+        if (icon !== null) {
+            Glide.with(this).load(icon).into(imageView)
+        }
+
+        hesapla.setOnClickListener {
+            isChecked = true
+            if(!radioButton1.isChecked && !radioButton2.isChecked && !radioButton3.isChecked){
+                Toast.makeText(baseContext, "Hamburger Menüden Seçim Yapmalısınız", Toast.LENGTH_SHORT).show()
+                isChecked = false
+            }
+            if(!radioButton4.isChecked && !radioButton5.isChecked && !radioButton6.isChecked){
+                Toast.makeText(baseContext, "Hamburgerden Seçim Yapmalısınız", Toast.LENGTH_SHORT).show()
+                isChecked = false
+            }
+
+            if(isChecked) {
+                if (radioButton1.isChecked) {
+                    toplam_ucret += 10
+                } else if (radioButton2.isChecked) {
+                    toplam_ucret += 20
+                } else {
+                    toplam_ucret += 30
+                }
+
+                if (radioButton4.isChecked) {
+                    toplam_ucret += 40
+                } else if (radioButton5.isChecked) {
+                    toplam_ucret += 50
+                } else {
+                    toplam_ucret += 60
+                }
+
+                ucret.isVisible = true
+                ucret.text = "Ödemeniz gereken ücret " + toplam_ucret.toString() + "TL"
+                toplam_ucret = 0
+            }
+        }
+
     }
 }
